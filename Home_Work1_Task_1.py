35b960a06dcb94695d02c6b18d1ea791d70c8429
msulym <msulym@llnw.com>
2019-05-06 14:34:39 +0300

    adding home work 1

---
+++ Home_Work1_Task_1.py
@@ -0,0 +1,11 @@
+var_int=10
+var_float=8.4
+var_str="No"
+big_int=var_int*3.5
+var_float-1
+var_int/var_float
+big_int/var_float
+print (var_int)
+print (var_float)
+print (var_str)
+print(var_str+var_str+"Yes" *3)
---
+++ Home_Work1_Task_2.py
@@ -0,0 +1,6 @@
+name=input("What is your name?")
+age=input("How old are you?")
+home=input("Where do you live?")
+print("Hello,  ",name + " !")
+print("Your age is ",age)
+print("You live in ",home)
